bc9592fc5ce797865bd3e0282b843746d475d4ea
mederic mesle <mederic.mesle@gmail.com>
2022-04-22 09:00:46 +0200

    root to set up to point at home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,14 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 Rails.start()
 Turbolinks.start()
+
+import "controllers"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'users#home'
  get 'home', to: 'users#home'
  get 'contact', to: 'users#home'
 end
